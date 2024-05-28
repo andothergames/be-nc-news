343c96c72ce14a813ea77c8eea8038eaa1d300bc
Joy <32204909+andothergames@users.noreply.github.com>
2024-05-28 15:11:03 +0100

    03 complete get api tests and functionality

--- app/app.js
+++ app/app.js
@@ -1,19 +1,10 @@
 const { getTopics } = require("../controllers/topics-controllers");
+const { getEndpoints } = require("../controllers/api-controllers");
 
 const express = require("express");
 const app = express();
 
+app.get("/api", getEndpoints);
 app.get("/api/topics", getTopics);
-
-app.use((err, req, res, next) => {
-  if (err.status && err.mssg) {
-    res.status(err.status).send({ msg: err.msg });
-  }
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "Bad request" });
-  } else {
-    next();
-  }
-});
 
 module.exports = app;
